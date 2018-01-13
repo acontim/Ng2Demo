e4df257921787a758cf0708041530a37385e0992
Alfonso Conti <alfonso.conti@transfesa.com>
2018-01-13 17:43:58 +0100

    add product pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import {HeaderComponent} from './header/header.component';
 import {ListComponent} from './list/list.component';
 import {DataService} from './data.service';
+import {ProductPipe} from './pipes/product.pipe';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     ListComponent,
+    ProductPipe
   ],
   imports: [
     CommonModule,
@@ -20,6 +22,7 @@ import {DataService} from './data.service';
     FormsModule,
     HeaderComponent,
     ListComponent,
+    ProductPipe
   ],
   providers: [DataService],
 })
