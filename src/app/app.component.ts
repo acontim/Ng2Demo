a91ab8bf2ddaa57e96402dac81e0143f94e53fd6
Alfonso Conti <alfonso.conti@transfesa.com>
2018-03-18 20:43:26 +0100

    Remove functions from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,4 @@ export class AppComponent {
   public header = 'supershop';
   public footer = 'supershop';
   public date = Date.now();
-  public items: Array<Item>;
-  private reverse = false;
-
-  constructor (private _dataService: DataService) {
-  }
-
-  generateList() {
-    if (this.reverse) {
-      this.items.reverse();
-      this.reverse = false;
-    }
-  }
-
-  reverseList() {
-    this.reverse = !this.reverse;
-    this.items.reverse();
-  }
 }
